771601c9f685dce5f501157414931164f67ee06f
aryan210905 <aryan2109shah@gmail.com>
2025-12-18 21:38:04 +0530

    Updated router codes

--- backend/fastapi_backend/app/routers/mcq_generation.py
+++ backend/fastapi_backend/app/routers/mcq_generation.py
@@ -3,7 +3,7 @@ from app.schemas.mcq_generation import (
     MCQGenerationRequest,
     MCQGenerationResponse
 )
-from app.services.mcq_generation_service import MCQGenerationService
+from app.services.mcq_generation_service import generation_service
 
 
 router = APIRouter(prefix="/mcqs", tags=["MCQ Generation"])
@@ -11,7 +11,7 @@ router = APIRouter(prefix="/mcqs", tags=["MCQ Generation"])
 
 @router.post("/generate", response_model=MCQGenerationResponse)
 async def generate_mcqs(payload: MCQGenerationRequest):
-    response = MCQGenerationService.generate_mcqs_service(payload)
+    response = generation_service.generate_mcqs_service(payload)
 
     if not response:
         raise HTTPException(
